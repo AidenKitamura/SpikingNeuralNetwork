3199d1d8085812ee86ce80207d3d864eb98a7cc0
AidenKitamura <kokojj123@gmail.com>
2021-07-31 17:34:47 +0800

    New extractors and update on readme

--- models/extractors/SimpleCNN.py
+++ models/extractors/SimpleCNN.py
@@ -3,11 +3,11 @@ import torch.nn as nn
 
 class SimpleCNN(nn.Module):
     """ Class Summary
-    This SimpleCNN backbone contains 6 layers,
+    This SimpleCNN backbone contains 4 layers,
     each of which uses a ReLU activation and a
     3*3 kernel. A max pooling of size 2*2 is 
-    applied after every 2 CNN layers. A drop-
-    out layer is applied at the last layer
+    applied after every CNN layer. A dropout
+    layer is applied at the last layer
     
     Parameters:
     - in_channel: input data channels
@@ -17,43 +17,39 @@ class SimpleCNN(nn.Module):
       image, in the form of W*H
     """
 
-    def __init__(self, in_channels, input_size, output_dim=256, **args):
+    def __init__(self, in_channels, output_dim=256, **args):
         super(SimpleCNN, self).__init__()
         self.args = args
         self.output_dim = output_dim
         self.input_size = input_size
-        for iteration_num in range(3):
-            self.input_size = (int(self.input_size[0] / 2),\
-                int(self.input_size[1] / 2))
-        self.output_size = self.input_size
-        self.model = nn.Sequential(
-            nn.Conv2d(in_channels = in_channels, out_channels = 2,\
-                kernel_size = (3, 3), stride = (1, 1), padding = (1, 1)),
-            nn.ReLU(),
-            nn.Conv2d(in_channels = 2, out_channels = 4,\
-                kernel_size = (3, 3), stride = (1, 1), padding = (1, 1)),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size = (2, 2)),
-            nn.Conv2d(in_channels = 4, out_channels = 8,\
-                kernel_size = (3, 3), stride = (1, 1), padding = (1, 1)),
-            nn.ReLU(),
-            nn.Conv2d(in_channels = 8, out_channels = 16,\
-                kernel_size = (3, 3), stride = (1, 1), padding = (1, 1)),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size = (2, 2)),
-            nn.Conv2d(in_channels = 16, out_channels = 32,\
-                kernel_size = (3, 3), stride = (1, 1), padding = (1, 1)),
-            nn.ReLU(),
-            nn.Conv2d(in_channels = 32, out_channels = 64,\
-                kernel_size = (3, 3), stride = (1, 1), padding = (1, 1)),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size = (2, 2)),
-            nn.Flatten(),
-            nn.Dropout(p=0.9),
-            nn.Linear(64 * self.output_size[0] * self.output_size[1],\
-                output_dim)
-        )
+
+        self.conv1 = nn.Conv2d(in_channels = in_channels, out_channels = 4,\
+                kernel_size = (3, 3), stride = (1, 1), padding = (1, 1))
+        self.conv2 = nn.Conv2d(in_channels = 4, out_channels = 16,\
+                kernel_size = (3, 3), stride = (1, 1), padding = (1, 1))
+        self.conv3 = nn.Conv2d(in_channels = 16, out_channels = 64,\
+                kernel_size = (3, 3), stride = (1, 1), padding = (1, 1))
+        self.conv4 = nn.Conv2d(in_channels = 64, out_channels = 256,\
+                kernel_size = (3, 3), stride = (1, 1), padding = (1, 1))
+        self.dropout = nn.Dropout(p = 0.9)
+        self.linear = nn.Linear(256, output_dim)
 
     def forward(self, x):
-        out = self.model(x)
+        out = self.conv1(x)
+        out = nn.ReLU()(out)
+        out = nn.MaxPool2d(kernel_size = (2, 2))
+        out = self.conv2(out)
+        out = nn.ReLU()(out)
+        out = nn.MaxPool2d(kernel_size = (2, 2))
+        out = self.conv3(out)
+        out = nn.ReLU()(out)
+        out = nn.MaxPool2d(kernel_size = (2, 2))
+        out = self.conv4(out)
+        out = nn.ReLU()(out)
+        out = nn.MaxPool2d(kernel_size = (2, 2))
+        out = nn.Flatten()(out)
+        out = self.dropout(out)
+        out = self.linear(out)
+        out = nn.Softmax(dim = 0)(out)
+
         return out
--- models/extractors/SpikingCNN.py
+++ models/extractors/SpikingCNN.py
@@ -1,103 +1,66 @@
-import argparse
 import torch
+import torchvision
+from extractors.modules import SpikingActivation, TemporalAvgPool
 import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
 
-from torchvision import datasets, transforms
 
-import numpy as np
-import math as m
-from SpikingLayer import SpikingLayer, SpikingVextLayer, poisson_spikes
-
-class SpikingConvNet(nn.Module):
+class SpikingCNN(nn.Module):
+    """ Class Summary
+    This SpikingCNN backbone contains 6 layers,
+    each of which uses a ReLU activation and a
+    3*3 kernel. A max pooling of size 2*2 is 
+    applied after every 2 CNN layers. A drop-
+    out layer is applied at the last layer
     
-    def __init__(self, Nin, Nout, Nsp, t1, t2, beta=5, scale=1):
-        super(SpikingConvNet, self).__init__()
-        self.Nsp = Nsp
-        self.Nout = Nout
-        self.Nin = Nin
-        self.Nhid1 = Nin
-        self.Nhid2 = 600
-        self.scale = scale
-        # self.conv1 = nn.Conv2d(1, 4, (5,5), stride=2, padding=2)
-        # Need to change this coefficient
-        self.l1 = nn.Linear(self.Nhid2, self.Nout, bias=None)
-        # self.conv2 = nn.Conv2d(4, 6, (5,5), stride=1, padding=0)
-        self.conv1 = nn.Conv2d(in_channels = 1, out_channels = 2,\
-                kernel_size = (3, 3), stride = (1, 1), padding = (1, 1))
-        self.conv2 = nn.Conv2d(in_channels = 2, out_channels = 4,\
-                kernel_size = (3, 3), stride = (1, 1), padding = (1, 1))
-        self.conv3 = nn.Conv2d(in_channels = 4, out_channels = 8,\
-                kernel_size = (3, 3), stride = (1, 1), padding = (1, 1))
-        self.conv4 = nn.Conv2d(in_channels = 8, out_channels = 16,\
-                kernel_size = (3, 3), stride = (1, 1), padding = (1, 1))
-        self.conv5 = nn.Conv2d(in_channels = 16, out_channels = 32,\
-                kernel_size = (3, 3), stride = (1, 1), padding = (1, 1))
-        self.conv6 = nn.Conv2d(in_channels = 32, out_channels = 64,\
-                kernel_size = (3, 3), stride = (1, 1), padding = (1, 1))
+    Parameters:
+    - in_channel: input data channels
+    - output_dim: latent size of linear layer
+      to be fed to the predictor
+    - input_size: the input dimension of the 
+      image, in the form of W*H
+    """
 
-        self.sl1 = SpikingLayer(t1, beta=beta)
-        self.sl2 = SpikingLayer(t1, beta=beta)
-        self.sl3 = SpikingLayer(t1, beta=beta)
-        self.sl4 = SpikingLayer(t1, beta=beta)
-        self.sl5 = SpikingLayer(t1, beta=beta)
-        self.sl6 = SpikingLayer(t1, beta=beta)
-        self.sl7 = SpikingLayer(t2, beta=beta)
+    def __init__(self, in_channels, output_dim = 10, frequency = 10, non_spiking_model = None, **args):
+        super(SpikingCNN, self).__init__()
+        self.args = args
+        self.output_dim = output_dim
+        self.frequency = frequency
+        self.dt = 1 / self.frequency
 
-    def forward(self, x, device):
-        s1 = torch.zeros(x.shape[0], 2, 28, 28).to(device)
-        v1 = torch.zeros(x.shape[0], 2, 28, 28).to(device)
-        s2 = torch.zeros(x.shape[0], 4, 28, 28).to(device)
-        v2 = torch.zeros(x.shape[0], 4, 28, 28).to(device)
-        s3 = torch.zeros(x.shape[0], 8, 14, 14).to(device)
-        v3 = torch.zeros(x.shape[0], 8, 14, 14).to(device)
-        s4 = torch.zeros(x.shape[0], 16, 14, 14).to(device)
-        v4 = torch.zeros(x.shape[0], 16, 14, 14).to(device)
-        s5 = torch.zeros(x.shape[0], 32, 7, 7).to(device)
-        v5 = torch.zeros(x.shape[0], 32, 7, 7).to(device)
-        s6 = torch.zeros(x.shape[0], 64, 7, 7).to(device)
-        v6 = torch.zeros(x.shape[0], 64, 7, 7).to(device)
+        if non_spiking_model == None:
+            self.conv1 = nn.Conv2d(in_channels = in_channels, out_channels = 4,\
+                    kernel_size = (3, 3), stride = (1, 1), padding = (1, 1))
+            self.conv2 = nn.Conv2d(in_channels = 4, out_channels = 16,\
+                    kernel_size = (3, 3), stride = (1, 1), padding = (1, 1))
+            self.conv3 = nn.Conv2d(in_channels = 16, out_channels = 64,\
+                    kernel_size = (3, 3), stride = (1, 1), padding = (1, 1))
+            self.conv4 = nn.Conv2d(in_channels = 64, out_channels = 256,\
+                    kernel_size = (3, 3), stride = (1, 1), padding = (1, 1))
+        else:
+            self.conv1 = non_spiking_model.conv1
+            self.conv2 = non_spiking_model.conv2
+            self.conv3 = non_spiking_model.conv3
+            self.conv4 = non_spiking_model.conv4
 
-        s7 = torch.zeros(x.shape[0], self.Nout).to(device)
-        v7 = torch.zeros(x.shape[0], self.Nout).to(device)
-        nsp = torch.zeros(x.shape[0], self.Nout).to(device)
+        self.dropout = nn.Dropout(p = 0.9)
+        self.linear = nn.Linear(256, output_dim)
 
-        for i in range(self.Nsp):
-            xi = poisson_spikes(x,self.scale).to(device)
+    def forward(self, x):
+        out = self.conv1(x)
+        out = SpikingActivation(nn.ReLU(), dt = self.dt, spiking_aware_training = False)
+        out = nn.MaxPool2d(kernel_size = (2, 2))
+        out = self.conv2(out)
+        out = SpikingActivation(nn.ReLU(), dt = self.dt, spiking_aware_training = False)
+        out = nn.MaxPool2d(kernel_size = (2, 2))
+        out = self.conv3(out)
+        out = SpikingActivation(nn.ReLU(), dt = self.dt, spiking_aware_training = False)
+        out = nn.MaxPool2d(kernel_size = (2, 2))
+        out = self.conv4(out)
+        out = SpikingActivation(nn.ReLU(), dt = self.dt, spiking_aware_training = False)
+        out = nn.MaxPool2d(kernel_size = (2, 2))
+        out = nn.Flatten()(out)
+        out = self.dropout(out)
+        out = self.linear(out)
+        out = nn.Softmax(dim = 0)(out)
 
-            xi = self.conv1(xi)
-            s1, v1 = self.sl1(xi, s1, v1)
-            s1 = nn.ReLU()(s1)
-
-            xi = self.conv2(s1)
-            s2, v2 = self.sl2(xi, s2, v2)
-            s2 = nn.ReLU()(s2)
-
-            s2 = nn.MaxPool2d(kernel = (2, 2))(s2)
-
-            xi = self.conv3(s2)
-            s3, v3 = self.sl3(xi, s3, v3)
-            s3 = nn.ReLU()(s3)
-
-            xi = self.conv4(s3)
-            s4, v4 = self.sl4(xi, s4, v4)
-            s4 = nn.ReLU()(s4)
-
-            s4 = nn.MaxPool2d(kernel = (2, 2))(s4)
-
-            xi = self.conv1(xi)
-            s5, v5 = self.sl5(xi, s5, v5)
-            s5 = nn.ReLU()(s5)
-
-            xi = self.conv2(s5)
-            s6, v6 = self.sl6(xi, s6, v6)
-            s6 = nn.ReLU()(s6)
-
-            s6 = nn.MaxPool2d(kernel = (2, 2))(s6)
-
-            xi = s6.view(s6.shape[0],-1)
-            xi2 = self.l1(xi)
-            s7, v7 = self.sl7(xi2, s7, v7)
-            nsp += s3
-        return nsp
+        return out
